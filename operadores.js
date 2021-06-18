01c9ae425d350acca677679c43ad597ff410e247
Valeush <valeushuaia@hotmail.com>
2021-06-18 15:35:59 -0300

    primer commit

---
+++ operadores.js
@@ -0,0 +1,13 @@
+var condicionUno = (1 == "1");
+var condicionDos = (1 === "1");
+var condicionTres = (0 || 1);
+var condicionCuatro = (true && 1);
+var condicionCinco = (1 - 1) && (2 + 2);
+var condicionSeis = (1 - 1) || (2 + 2);
+
+console.log (condicionUno);
+console.log (condicionDos);
+console.log (condicionTres);
+console.log (condicionCuatro);
+console.log (condicionCinco);
+console.log (condicionSeis);
